440fd2131f11c334e85ff59ba11eef9f1f832706
CoachCarle <148357602+CoachCarle@users.noreply.github.com>
2026-09-15 17:55:02 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE = "palestra-v1";
+const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+  );
+  self.clients.claim();
+});
+
+// Network-first: mentre modifichi la scheda vuoi sempre l'ultima versione online;
+// se sei offline (es. spogliatoio senza rete) usa l'ultima copia salvata.
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
